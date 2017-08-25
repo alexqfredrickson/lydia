0129071a82496e1f2c875e1faf6af03f442e8c07
Alex <alex.q.fredrickson@gmail.com>
2017-08-24 23:26:44 -0400

    Create lydia.py

---
+++ lydia.py
@@ -0,0 +1,117 @@
+from eyed3 import id3
+import os
+import re
+
+
+class Folder:
+    def __init__(self, rootdir, folder_name):
+        self.rootdir = rootdir
+        self.fully_qualified_path = rootdir + "\\" + folder_name
+        self.name = folder_name
+        self.first_mp3 = FirstMp3(self.fully_qualified_path)
+        self.album_year = None
+
+    def name_is_valid(self):
+        return self.name_is_at_least_5_chars() \
+               and self.name_has_uppercase_letters() is False \
+               and self.name_starts_with_year_and_space()
+
+    def name_is_at_least_5_chars(self):
+        return len(self.name) > 5
+
+    def name_has_uppercase_letters(self):
+        return any(x.isupper() for x in self.name)
+
+    def name_starts_with_year_and_space(self):
+        if self.name[0].isdigit() \
+               and self.name[1].isdigit() \
+               and self.name[2].isdigit() \
+               and folder[3].isdigit() \
+               and folder[4].isspace() \
+               and folder[5] == "-":
+            first_two_chars = self.name[0] + self.name[1]
+            if first_two_chars in ("19", "20"):
+                return True
+        else:
+            return False
+
+    def guess_album_year(self):
+        if str.isdigit(self.name[0]) is False and re.match('^.[\d]{4}', self.name):
+            print("FYI - I think the album year might be {}.".format(self.name[1:5]))
+            return self.name[1:5]
+
+        elif re.match('^[\d]{4}', self.name):
+            print("FYI - I think the album year might be {}.".format(self.name[0:4]))
+            return self.name[0:4]
+
+
+class FirstMp3:
+    def __init__(self, folder_fully_qualified_path):
+        self.name = os.listdir(folder_fully_qualified_path)[0]
+        self.fully_qualified_path = folder_fully_qualified_path + "\\" + self.name
+        self.file_tag = id3.Tag()
+        self.file_tag.parse(self.fully_qualified_path)
+
+
+class Utilities:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def prompt_rename(old_dir_name, new_dir_name):
+        print("Would you like to change '{}' to '{}'?".format(old_dir_name, new_dir_name))
+        user_input = input().lower()
+
+        if user_input in ("y", "yes"):
+            print("Okay, will do!")
+            os.rename(old_dir_name, new_dir_name)
+            # TODO: when files are renamed, change the folder references to the old fq name
+            print("Succesfully renamed '{}' to '{}'.".format(old_dir_name, new_dir_name))
+
+rootdir = "C:\my horribly disorganized music folder"
+
+for folder in os.listdir(rootdir):
+    f = Folder(rootdir, folder)
+
+    if f.name_is_valid():
+        print("'{}' looks legit!".format(f.name))
+    else:
+        if f.name_is_at_least_5_chars() is False:
+            print("'{}' has way too short of a name to signify anything really.".format(folder))
+            continue
+
+        if f.name_has_uppercase_letters():
+            print("No big deal but it looks like '{}' has uppercase letters.".format(f.name))
+
+            Utilities.prompt_rename(
+                f.fully_qualified_path, os.path.join(os.path.join(rootdir, f.name.lower()))
+            )
+
+        if f.name_starts_with_year_and_space() is False:
+            print("'{}' doesn't start with 'YYYY - '. Let's attempt to infer it from the first .mp3's ID3 tag.".format(
+                f.name))
+
+            if os.path.isdir(f.first_mp3.fully_qualified_path) is False:
+                print("Examining {}.".format(f.first_mp3.fully_qualified_path))
+
+                if f.first_mp3.file_tag.recording_date is not None and f.first_mp3.file_tag.album is not None:
+                    print("The old folder name is '{}'.".format(f.fully_qualified_path))
+
+                    Utilities.prompt_rename(
+                        f.fully_qualified_path,
+                        os.path.join(os.path.join(rootdir, str(f.first_mp3.file_tag.recording_date)
+                                                  + " - " + f.first_mp3.file_tag.album.lower()))
+                    )
+
+                else:
+                    if f.first_mp3.file_tag.recording_date is None and f.first_mp3.file_tag.album is None:
+                        print("Looks like the .mp3s in here don't have album recording dates, nor album names, "
+                              "so I'm going to skip this folder for now.")
+                    elif f.first_mp3.file_tag.recording_date is None:
+                        print("Looks like this .mp3 doesn't have an album recording date.")
+                        f.album_year = f.guess_album_year()
+                    elif f.first_mp3.file_tag.recording_date is None:
+                        print("Looks like this .mp3 doesn't have an album name.")
+                    else:
+                        print("Something really fucked up happened.")
+    print("\n")
