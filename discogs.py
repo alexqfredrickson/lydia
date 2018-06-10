1a8fc490b02ea75afe0769993409c65871e2419f
aqf <alexqfredrickson@gmail.com>
2018-06-10 14:33:26 -0400

    discogs.py updates

--- discogs.py
+++ discogs.py
@@ -1,18 +1,33 @@
 import warnings
 import discogs_client
 from models import LydiaConfig
+import time
 
 
-class Discogs:
-    def __init__(self):
+class DiscogsApi:
+    def __init__(self, verbose=False):
+        """
+        Initializes a DiscogsApi object.
+        """
+
         self.controller = discogs_client.Client(
             'lydia/0.0.1-alpha',
             user_token=LydiaConfig().discogs_user_token
         )
 
-    @staticmethod
-    def search_artist(artist_name):
-        artist_results = discogs.controller.search(artist_name, type='artist')
+        self.verbose = verbose
+
+    def get_artist(self, artist_name):
+        """
+        Searches the Discogs database for information about a given artist.
+        :param artist_name: An artist name.
+        :return: A Discogs Artist object.
+        """
+
+        if self.verbose:
+            print("Searching Discogs database for {}...".format(artist_name))
+
+        artist_results = self.controller.search(artist_name, type='artist')
 
         if len(artist_results) > 1:
             warnings.warn(message="The Discogs search for artist '{}' returned multiple artists: {}"
@@ -23,9 +38,17 @@ class Discogs:
 
         return artist_results[0]
 
-    @staticmethod
-    def search_album(album_name):
-        album_results = discogs.controller.search(album_name, type='release')
+    def get_album(self, album_name):
+        """
+        Searches the Discogs database for information about a given album.
+        :param album_name: An album name.
+        :return: A Discogs Release object.
+        """
+
+        if self.verbose:
+            print("Searching Discogs database for {}...".format(album_name))
+
+        album_results = self.controller.search(album_name, type='release')
 
         if len(album_results) > 1:
             warnings.warn(message="The Discogs search for album '{}' returned multiple albums: {}"
@@ -35,3 +58,44 @@ class Discogs:
 
         return album_results[0]
 
+    def get_artist_releases(self, artist_name):
+        """
+        Searches the Discogs database for information about a given artist's albums.
+
+        :param artist_name: An album name.
+        :return: A list of Discogs Release objects.
+        :rtype: list
+        """
+
+        if self.verbose:
+            print("Searching Discogs database for {} releases...".format(artist_name))
+
+        releases = self.get_artist(artist_name).releases
+        return [r for r in releases]
+
+    def get_artist_songs(self, artist_name):
+        """
+        Searches the Discogs database for a list of the artist's songs.
+
+        :param artist_name: An artist name.
+        :return: A list containing all of an artist's songs.
+        :rtype: list
+        """
+
+        songs = []
+        releases = self.get_artist_releases(artist_name)
+
+        for release in releases:
+            for track in release.tracklist:
+                print("{} - {} added to master tracklist.".format(track.data["title"], release.data["title"]))
+                songs.append(track)
+                time.sleep(1)
+
+        return songs
+
+
+discogs = DiscogsApi(verbose=True)
+gun_club_songs = discogs.get_artist_songs("The Gun Club")
+
+for s in gun_club_songs:
+    print(s)
